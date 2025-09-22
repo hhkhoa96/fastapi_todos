69aa7fa7049c85575a283aa700bf8f443892dc63
Khoa Hoang Hoa <khoa.hoanghoa@nashtechglobal.com>
2025-09-22 16:32:06 +0700

    Add fields for existed company table

--- schemas/company.py
+++ schemas/company.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, CheckConstraint
 
 from .base_entity import Base, BaseEntity
 
@@ -7,3 +7,9 @@ class Company(Base, BaseEntity):
     __tablename__ = "companies"
 
     name = Column(String, nullable=False)
+    description = Column(String)
+    rating = Column(
+        Integer,
+        CheckConstraint("value BETWEEN 1 AND 5"),
+        nullable=False
+    )
