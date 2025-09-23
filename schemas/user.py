26c6624e380f0fd80b710c5bb7f67a81b4aa7ccd
Khoa Hoang Hoa <khoa.hoanghoa@nashtechglobal.com>
2025-09-23 10:57:51 +0700

    Alembic create users table

---
+++ schemas/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Boolean, ForeignKey, UUID
+
+from .base_entity import Base, BaseEntity
+
+
+class User(Base, BaseEntity):
+    __tablename__ = "users"
+
+    username = Column(String, nullable=False, unique=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    is_admin = Column(Boolean, default=False, nullable=False)
+    is_superuser = Column(Boolean, default=False, nullable=False)
+
+    company_id = Column(UUID(), ForeignKey("companies.id"))
